d93f32c67264bafb81bc0c5d4d6aca34c2bdc480
Mark Lopez <mark_lopez@icloud.com>
2021-05-22 19:43:25 -0500

    added in comments

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+//setting the minutes on  the timer and how fast the countdown will be
 function clock() {
     var myTimer = setInterval(myClock, 1000);
-    var c = 60;
-
+    var count = 60;
+// created countdown for timer with a times up message when timer hits 0
     function myClock() {
-      document.getElementById("demo").innerHTML = --c;
-      if (c == 0) {
+      document.getElementById("countdown").innerHTML = --count;
+      if (count == 0) {
         clearInterval(myTimer);
         alert("Times Up!");
       }
     }
   }
-
-
